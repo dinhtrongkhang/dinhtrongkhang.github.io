57c04eb3d3ee6d7eb991dc56c5183bd6b47c8d56
dinhtrongkhang <69962473+dinhtrongkhang@users.noreply.github.com>
2020-12-13 16:59:43 +0700

    khang dinh

---
+++ khangdt/assets/js/bai7.js
@@ -0,0 +1,8 @@
+function dropDownNav() {
+    document.getElementById("header__top-mobile").style.display = "block";
+}
+  function closeNav() {
+    document.getElementById("header__top-mobile").style.display = "none";
+}
+
+  
