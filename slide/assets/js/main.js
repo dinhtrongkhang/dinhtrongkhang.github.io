4a54026eaf94b5921c627bd88f755186e5603cc9
dinhtrongkhang <69962473+dinhtrongkhang@users.noreply.github.com>
2020-12-24 21:39:00 +0700

    khang

--- slide/assets/js/main.js
+++ slide/assets/js/main.js
@@ -7,9 +7,38 @@ function myFunction() {
     }
 }
 
-
-$('.main-slider').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
+$('.slider').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: ' <span class="prev-Arrow"><i class="fas fa-angle-double-right"></i></span>',
+    nextArrow: '<span class="next-Arrow"><i class="fas fa-angle-double-left"></i></span>',
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
 });
