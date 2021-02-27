2e1f53fc0f56b3be004b08a8036f58e5e0b5787c
dinhtrongkhang <69962473+dinhtrongkhang@users.noreply.github.com>
2021-02-27 09:28:01 +0700

    khang dinh

--- khangdt/assets/js/bai6.js
+++
@@ -1,8 +0,0 @@
-function dropDownNav() {
-    document.getElementById("header__top-mobile").style.display = "block";
-}
-  function closeNav() {
-    document.getElementById("header__top-mobile").style.display = "none";
-}
-
-  
