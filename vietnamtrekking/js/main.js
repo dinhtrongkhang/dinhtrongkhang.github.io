6673ea7fadc189a5885c959e39d9a9deeccbe087
dinhtrongkhang <69962473+dinhtrongkhang@users.noreply.github.com>
2021-03-06 09:32:06 +0700

    khang dinh

--- vietnamtrekking/js/main.js
+++ vietnamtrekking/js/main.js
@@ -80,72 +80,42 @@ $('.tour__slide').slick({
 });
 
 
-// slide tour nuoc ngoai
 
-// $('.tour__dulichnuocngoai').slick({
-//     speed: 1000,
-//     prevArrow: '<span class="prev-arrow-nuocngoai slide-arrow"><i class="fas fa-angle-left"></i></span>',
-//     nextArrow: '<span class="next-arrow-nuocngoai slide-arrow"><i class="fas fa-angle-right"></i></span>',
-//     infinite: false,
-//     speed: 300,
-//     slidesToShow: 4,
-//     slidesToScroll: 2,
-//     responsive: [{
-//         breakpoint: 1024,
-//         settings: {
-//             slidesToShow: 3,
-//             slidesToScroll: 3,
-//             infinite: true,
-//         }
-//     }, {
-//         breakpoint: 768,
-//         settings: {
-//             slidesToShow: 2,
-//             slidesToScroll: 2
-//         }
-//     }, {
-//         breakpoint: 576,
-//         settings: {
-//             slidesToShow: 1,
-//             slidesToScroll: 1
-//         }
-//     }]
-// });
 
 
 // tour chi tiet
 
-// $('.tour__chitiet-box-img').slick({
-//     dots: true,
-//     infinite: false,
-//     speed: 1000,
-//     prevArrow: '<span class="prev-arrow-tourchitiet slide-arrow"><i class="fas fa-angle-left"></i></span>',
-//     nextArrow: '<span class="next-arrow-tourchitiet slide-arrow"><i class="fas fa-angle-right"></i></span>',
-//     infinite: false,
-//     speed: 300,
-//     slidesToShow: 1,
-//     slidesToScroll: 2,
-//     responsive: [{
-//         breakpoint: 1024,
-//         settings: {
-//             slidesToShow: 1,
-//             slidesToScroll: 1,
-//             infinite: true,
-//         }
-//     }, {
-//         breakpoint: 768,
-//         settings: {
-//             slidesToShow: 1,
-//             slidesToScroll: 1
-//         }
-//     }, {
-//         breakpoint: 576,
-//         settings: {
-//             slidesToShow: 1,
-//             slidesToScroll: 1
-//         }
-//     }]
-// });
+$('.tour__chitiet-box-img').slick({
+    dots: true,
+    infinite: false,
+    speed: 1000,
+    prevArrow: '<span class="prev-arrow-tourchitiet slide-arrow"><i class="fas fa-angle-left"></i></span>',
+    nextArrow: '<span class="next-arrow-tourchitiet slide-arrow"><i class="fas fa-angle-right"></i></span>',
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 2,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+        }
+    }, {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    }, {
+        breakpoint: 576,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    }]
+});
 
 
 // thông tin đặt tour
