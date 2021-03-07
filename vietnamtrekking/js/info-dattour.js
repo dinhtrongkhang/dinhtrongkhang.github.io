fc53c2ebd113a595c78d7b5a479d5bc502eb26b9
dinhtrongkhang <69962473+dinhtrongkhang@users.noreply.github.com>
2021-03-07 08:36:39 +0700

    khang dinh

---
+++ vietnamtrekking/js/info-dattour.js
@@ -0,0 +1,56 @@
+const form = document.getElementById('thongtin');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('tel');
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    // trim to remove the whitespaces
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+
+    if (usernameValue === '') {
+        setErrorFor(username, 'Vui lòng nhập trường này');
+    } else {
+        setSuccessFor(username);
+    }
+
+    if (emailValue === '') {
+        setErrorFor(email, 'Email không đúng');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Not a valid email');
+    } else {
+        setSuccessFor(email);
+    }
+
+    if (passwordValue === '') {
+        setErrorFor(password, 'Số điện thoại không đúng');
+    } else {
+        setSuccessFor(password);
+    }
+
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
