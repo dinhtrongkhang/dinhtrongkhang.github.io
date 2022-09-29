9994e43033bf84c63396da79bfab5186560f3b3a
dinhtrongkhang <69962473+dinhtrongkhang@users.noreply.github.com>
2022-09-29 10:46:39 +0700

    khangdinh

---
+++ giahuyshop-2022-final/assets/js/script.js
@@ -0,0 +1,9 @@
+// $('.banner').slick({
+//     prevArrow: '<span class="prev-arrow slide-arrow"><i class="fas fa-angle-left"></i></span>',
+//     nextArrow: '<span class="next-arrow slide-arrow"><i class="fas fa-angle-right"></i></span>',
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 1,
+//     adaptiveHeight: true
+// });
