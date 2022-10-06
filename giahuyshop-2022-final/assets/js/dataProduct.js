86017cd89b22b8cd76acc6f33451f51257a99b7f
dinhtrongkhang <69962473+dinhtrongkhang@users.noreply.github.com>
2022-10-06 11:28:23 +0700

    khangdinh

---
+++ giahuyshop-2022-final/assets/js/dataProduct.js
@@ -0,0 +1,148 @@
+const babyProduct = [
+    {
+        src: "./assets/image/gh-baby-1.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 1
+    },
+    {
+        src: "./assets/image/gh-baby-2.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 2
+    },
+    {
+        src: "./assets/image/gh-baby-3.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 3
+    },
+    {
+        src: "./assets/image/gh-baby-4.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 4
+    },
+    {
+        src: "./assets/image/gh-baby-5.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 5
+    },
+    {
+        src: "./assets/image/gh-baby-6.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 6
+    },
+    {
+        src: "./assets/image/gh-baby-7.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 7
+    },
+    {
+        src: "./assets/image/gh-baby-8.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 8
+    },
+    {
+        src: "./assets/image/gh-baby-9.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 9
+    },
+    {
+        src: "./assets/image/gh-baby-10.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 10
+    },
+    {
+        src: "./assets/image/gh-baby-11.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 11
+    },
+    {
+        src: "./assets/image/gh-baby-12.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 11
+    },
+];
+
+const collectionProduct = [
+    {
+        src: "./assets/image/gh-baby-1.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 1
+    },
+    {
+        src: "./assets/image/gh-baby-2.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 2
+    },
+    {
+        src: "./assets/image/gh-baby-3.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 3
+    },
+    {
+        src: "./assets/image/gh-baby-4.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 4
+    },
+];
+
+const newProduct = [
+    {
+        src: "./assets/image/gh-baby-1.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 1
+    },
+    {
+        src: "./assets/image/gh-baby-2.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 2
+    },
+    {
+        src: "./assets/image/gh-baby-3.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 3
+    },
+    {
+        src: "./assets/image/gh-baby-4.webp",
+        Title: "T-shirt",
+        ActualPrice: "$10.00",
+        OldPrice: "$11.00",
+        id: 4
+    },
+];
--- giahuyshop-2022-final/assets/js/listProduct.js
+++ giahuyshop-2022-final/assets/js/listProduct.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
         margin: 0,
         nav: false,
         autoplay: true,
+        autoplayTimeout: 2000,
         responsive: {
             0: {
                 items: 1
@@ -16,9 +17,71 @@ $(document).ready(function () {
             }
         }
     })
+
+    $("#four-col-jq").on("click", function () {
+        alert("click vào ra 4 cột");
+        // $(".setion-listing").addClass( "four-listing" );
+        $(".setion-listing").removeClass("two-listing");
+    })
+
+    $("#two-col-jq").on("click", function () {
+        alert("click vào ra 2 cột");
+        $(".setion-listing").removeClass("four-listing");
+        $(".setion-listing").addClass("two-listing");
+    })
+
+    // Render List Product
+    document.querySelector(`#list-product--js`).innerHTML = babyProduct.map((value, key) =>
+        // console.log(value, "value");
+        // console.log(key, "key")
+        `<div class="col-12 col-sm-3 col-md-3 col-lg-3">
+<div class="list-product">
+    <img class="product-img" src=${value.src} />
+    <div class="product-title">${value.Title}</div>
+    <div class="product-price">
+        <div class="actual-price">${value.ActualPrice}</div>
+        <div class="old-price">${value.OldPrice}</div>
+    </div>
+    <button class="add-to-cart btn">Add To Cart</button>
+</div>
+</div>
+`
+    ).join('');
+
+    // Colletion Product
+    document.querySelector(`#collection-product--js`).innerHTML = collectionProduct.map((value, key) =>
+        // console.log(value, "value");
+        // console.log(key, "key")
+        `<div class="col-12 col-sm-3 col-md-3 col-lg-3">
+<div class="list-product">
+<img class="product-img" src=${value.src} />
+<div class="product-title">${value.Title}</div>
+<div class="product-price">
+    <div class="actual-price">${value.ActualPrice}</div>
+    <div class="old-price">${value.OldPrice}</div>
+</div>
+<button class="add-to-cart btn">Add To Cart</button>
+</div>
+</div>
+`
+    ).join('');
+
+    // New Product
+    document.querySelector(`#new-product--js`).innerHTML = newProduct.map((value, key) =>
+        // console.log(value, "value");
+        // console.log(key, "key")
+        `<div class="col-12 col-sm-3 col-md-3 col-lg-3">
+<div class="list-product">
+<img class="product-img" src=${value.src} />
+<div class="product-title">${value.Title}</div>
+<div class="product-price">
+    <div class="actual-price">${value.ActualPrice}</div>
+    <div class="old-price">${value.OldPrice}</div>
+</div>
+<button class="add-to-cart btn">Add To Cart</button>
+</div>
+</div>
+`
+    ).join('');
 });
-
-
-
-
 
