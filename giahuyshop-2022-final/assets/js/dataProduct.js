e9af0926440f660cac39843d112422ec3baae6c2
dinhtrongkhang <69962473+dinhtrongkhang@users.noreply.github.com>
2022-10-12 09:27:45 +0700

    khangdinh

--- giahuyshop-2022-final/assets/js/dataProduct.js
+++ giahuyshop-2022-final/assets/js/dataProduct.js
@@ -1,86 +1,86 @@
 const babyProduct = [
     {
         src: "./assets/image/gh-baby-1.webp",
-        Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        Title: "Áo Thun Dài Tay Nam",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 1
     },
     {
         src: "./assets/image/gh-baby-2.webp",
-        Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        Title: "Áo Thun Dài Tay Nam",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 2
     },
     {
         src: "./assets/image/gh-baby-3.webp",
-        Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        Title: "Áo Thun Dài Tay Nam",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 3
     },
     {
         src: "./assets/image/gh-baby-4.webp",
-        Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        Title: "Áo Thun Dài Tay Nữ",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 4
     },
     {
         src: "./assets/image/gh-baby-5.webp",
-        Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        Title: "Áo Polo Trẻ Em",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 5
     },
     {
         src: "./assets/image/gh-baby-6.webp",
-        Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        Title: "Áo Polo Trẻ Em",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 6
     },
     {
         src: "./assets/image/gh-baby-7.webp",
-        Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        Title: "Áo Polo Trẻ Em",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 7
     },
     {
         src: "./assets/image/gh-baby-8.webp",
-        Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        Title: "Áo Polo Trẻ Em",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 8
     },
     {
         src: "./assets/image/gh-baby-9.webp",
-        Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        Title: "Áo Polo Trẻ Em",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 9
     },
     {
         src: "./assets/image/gh-baby-10.webp",
-        Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        Title: "Áo Polo Trẻ Em",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 10
     },
     {
         src: "./assets/image/gh-baby-11.webp",
-        Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        Title: "Quần Kaki Trẻ Em",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 11
     },
     {
         src: "./assets/image/gh-baby-12.webp",
-        Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        Title: "Quần Short Bé Trai",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 12
     },
 ];
@@ -88,30 +88,30 @@ const babyProduct = [
 const collectionProduct = [
     {
         src: "./assets/image/gh-baby-1.webp",
-        Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        Title: "Áo Thun Dài Tay Nam",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 13
     },
     {
         src: "./assets/image/gh-baby-2.webp",
-        Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        Title: "Áo Thun Dài Tay Nam",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 14
     },
     {
         src: "./assets/image/gh-baby-3.webp",
-        Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        Title: "Áo Thun Dài Tay Nam",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 15
     },
     {
         src: "./assets/image/gh-baby-4.webp",
-        Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        Title: "Áo Thun Dài Tay Nữ",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 16
     },
 ];
@@ -120,29 +120,29 @@ const newProduct = [
     {
         src: "./assets/image/gh-baby-1.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 17
     },
     {
         src: "./assets/image/gh-baby-2.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 18
     },
     {
         src: "./assets/image/gh-baby-3.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 19
     },
     {
         src: "./assets/image/gh-baby-4.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 20
     },
 ];
@@ -151,85 +151,85 @@ const womenProduct = [
     {
         src: "./assets/image/gh-wm-1.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 21
     },
     {
         src: "./assets/image/gh-wm-2.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 22
     },
     {
         src: "./assets/image/gh-wm-3.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 23
     },
     {
         src: "./assets/image/gh-wm-4.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 24
     },
     {
         src: "./assets/image/gh-wm-5.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 26
     },
     {
         src: "./assets/image/gh-wm-6.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 27
     },
     {
         src: "./assets/image/gh-wm-7.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 28
     },
     {
         src: "./assets/image/gh-wm-8.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 29
     },
     {
         src: "./assets/image/gh-wm-9.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 30
     },
     {
         src: "./assets/image/gh-wm-10.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 31
     },
     {
         src: "./assets/image/gh-wm-11.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 32
     },
     {
         src: "./assets/image/gh-wm-12.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 33
     },
 ];
@@ -238,85 +238,85 @@ const menProduct = [
     {
         src: "./assets/image/gh-men-1.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 34
     },
     {
         src: "./assets/image/gh-men-2.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 34
     },
     {
         src: "./assets/image/gh-men-3.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 35
     },
     {
         src: "./assets/image/gh-men-4.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 36
     },
     {
         src: "./assets/image/gh-men-5.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 37
     },
     {
         src: "./assets/image/gh-men-6.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 38
     },
     {
         src: "./assets/image/gh-men-7.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 39
     },
     {
         src: "./assets/image/gh-men-8.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 40
     },
     {
         src: "./assets/image/gh-men-9.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 41
     },
     {
         src: "./assets/image/gh-men-10.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 42
     },
     {
         src: "./assets/image/gh-men-11.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 43
     },
     {
         src: "./assets/image/gh-men-12.webp",
         Title: "T-shirt",
-        ActualPrice: "$10.00",
-        OldPrice: "$11.00",
+        ActualPrice: "250.000đ",
+        OldPrice: "300.000đ",
         id: 44
     },
 ];
