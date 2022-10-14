6dc603fdc80a7c5e549f789a4503d9f2b81cc6fc
dinhtrongkhang <69962473+dinhtrongkhang@users.noreply.github.com>
2022-10-14 17:40:13 +0700

    khangdinh

--- giahuyshop-2022-v1/assets/js/script.js
+++ giahuyshop-2022-v1/assets/js/script.js
@@ -41,7 +41,7 @@ $(document).ready(function () {
             {
                 breakpoint: 1025,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 4,
                 },
             },
             {
