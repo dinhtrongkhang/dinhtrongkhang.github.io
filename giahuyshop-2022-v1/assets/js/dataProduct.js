f9c4f336e3cd1563295df012e3800f7647e9ee5b
dinhtrongkhang <69962473+dinhtrongkhang@users.noreply.github.com>
2022-10-14 17:02:38 +0700

    khangdinh

--- giahuyshop-2022-v1/assets/js/dataProduct.js
+++ giahuyshop-2022-v1/assets/js/dataProduct.js
@@ -2,85 +2,85 @@ const babyProduct = [
     {
         src: "./assets/image/gh-baby-1.webp",
         Title: "Áo Thun Dài Tay Nam",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 1
     },
     {
         src: "./assets/image/gh-baby-2.webp",
         Title: "Áo Thun Dài Tay Nam",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 2
     },
     {
         src: "./assets/image/gh-baby-3.webp",
         Title: "Áo Thun Dài Tay Nam",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 3
     },
     {
         src: "./assets/image/gh-baby-4.webp",
         Title: "Áo Thun Dài Tay Nữ",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 4
     },
     {
         src: "./assets/image/gh-baby-5.webp",
         Title: "Áo Polo Trẻ Em",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 5
     },
     {
         src: "./assets/image/gh-baby-6.webp",
         Title: "Áo Polo Trẻ Em",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 6
     },
     {
         src: "./assets/image/gh-baby-7.webp",
         Title: "Áo Polo Trẻ Em",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 7
     },
     {
         src: "./assets/image/gh-baby-8.webp",
         Title: "Áo Polo Trẻ Em",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 8
     },
     {
         src: "./assets/image/gh-baby-9.webp",
         Title: "Áo Polo Trẻ Em",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 9
     },
     {
         src: "./assets/image/gh-baby-10.webp",
         Title: "Áo Polo Trẻ Em",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 10
     },
     {
         src: "./assets/image/gh-baby-11.webp",
         Title: "Quần Kaki Trẻ Em",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 11
     },
     {
         src: "./assets/image/gh-baby-12.webp",
         Title: "Quần Short Bé Trai",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 12
     },
 ];
@@ -89,29 +89,29 @@ const collectionProduct = [
     {
         src: "./assets/image/gh-baby-1.webp",
         Title: "Áo Thun Dài Tay Nam",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 13
     },
     {
         src: "./assets/image/gh-baby-2.webp",
         Title: "Áo Thun Dài Tay Nam",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 14
     },
     {
         src: "./assets/image/gh-baby-3.webp",
         Title: "Áo Thun Dài Tay Nam",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 15
     },
     {
         src: "./assets/image/gh-baby-4.webp",
         Title: "Áo Thun Dài Tay Nữ",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 16
     },
 ];
@@ -120,29 +120,29 @@ const newProduct = [
     {
         src: "./assets/image/gh-baby-1.webp",
         Title: "T-shirt",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 17
     },
     {
         src: "./assets/image/gh-baby-2.webp",
         Title: "T-shirt",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 18
     },
     {
         src: "./assets/image/gh-baby-3.webp",
         Title: "T-shirt",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 19
     },
     {
         src: "./assets/image/gh-baby-4.webp",
         Title: "T-shirt",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 20
     },
 ];
@@ -151,85 +151,85 @@ const womenProduct = [
     {
         src: "./assets/image/gh-wm-1.webp",
         Title: "Áo Polo Nữ",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 21
     },
     {
         src: "./assets/image/gh-wm-2.webp",
         Title: "Áo Polo Nữ",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 22
     },
     {
         src: "./assets/image/gh-wm-3.webp",
         Title: "Áo Polo Nữ",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 23
     },
     {
         src: "./assets/image/gh-wm-4.webp",
         Title: "Áo Thun Dài Tay Nữ",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 24
     },
     {
         src: "./assets/image/gh-wm-5.webp",
         Title: "Áo T-Shirt Nữ",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 26
     },
     {
         src: "./assets/image/gh-wm-6.webp",
         Title: "Áo T-Shirt Nữ",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 27
     },
     {
         src: "./assets/image/gh-wm-7.webp",
         Title: "Áo T-Shirt Nữ",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 28
     },
     {
         src: "./assets/image/gh-wm-8.webp",
         Title: "Áo T-Shirt Nữ",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 29
     },
     {
         src: "./assets/image/gh-wm-9.webp",
         Title: "Áo T-Shirt Nữ",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 30
     },
     {
         src: "./assets/image/gh-wm-10.webp",
         Title: "Áo T-Shirt Nữ",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 31
     },
     {
         src: "./assets/image/gh-wm-11.webp",
         Title: "Áo Thun Nữ Đệm Vai Họa Tiết",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 32
     },
     {
         src: "./assets/image/gh-wm-12.webp",
         Title: "Áo T-Shirt Nữ",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 33
     },
 ];
@@ -238,85 +238,85 @@ const menProduct = [
     {
         src: "./assets/image/gh-men-1.webp",
         Title: "Áo Sơ Mi Nam Dài Tay",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 34
     },
     {
         src: "./assets/image/gh-men-2.webp",
         Title: "Áo Sơ Mi Nam Dài Tay",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 34
     },
     {
         src: "./assets/image/gh-men-3.webp",
         Title: "Áo Sơ Mi Nam Dài Tay",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 35
     },
     {
         src: "./assets/image/gh-men-4.webp",
         Title: "Áo Sơ Mi Nam Dài Tay",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 36
     },
     {
         src: "./assets/image/gh-men-5.webp",
         Title: "Áo T-Shirt Nam",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 37
     },
     {
         src: "./assets/image/gh-men-6.webp",
         Title: "Áo T-Shirt Nam",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 38
     },
     {
         src: "./assets/image/gh-men-7.webp",
         Title: "Áo T-Shirt Nam",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 39
     },
     {
         src: "./assets/image/gh-men-8.webp",
         Title: "Áo T-Shirt Nam",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 40
     },
     {
         src: "./assets/image/gh-men-9.webp",
         Title: "Áo T-Shirt Nam",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 41
     },
     {
         src: "./assets/image/gh-men-10.webp",
         Title: "Áo Sơ Mi Nam Ngắn Tay",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 42
     },
     {
         src: "./assets/image/gh-men-11.webp",
         Title: "Áo Sơ Mi Nam Ngắn Tay",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 43
     },
     {
         src: "./assets/image/gh-men-12.webp",
         Title: "Áo Sơ Mi Nam Ngắn Tay",
-        ActualPrice: "250.000đ",
-        OldPrice: "300.000đ",
+        ActualPrice: "250.000",
+        OldPrice: "300.000",
         id: 44
     },
 ];
