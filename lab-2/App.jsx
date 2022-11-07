2f899513c553fc2cd658cf8a871b0b9ea4cb7737
dinhtrongkhang <khangdinhtb@gmail.com>
2022-11-07 16:24:34 +0700

    khangdinhtb

--- lab-2/App.jsx
+++ lab-2/App.jsx
@@ -460,101 +460,226 @@
 
 
 
+// const App = () => {
+//   const reviews = [
+//     {
+//       id: "f78ff93a2f6723ba714b938d",
+//       avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/971.jpg",
+//       name: "Jorge Farrell",
+//       job: "Investor Marketing Producer",
+//       content:
+//         "Dignissimos libero magnam unde sapiente quaerat vel accusantium reprehenderit. Quam excepturi omnis. Non inventore facilis ipsum iure omnis quis sunt.",
+//     },
+//     {
+//       id: "b39e485adf7eac8ef7ccdb7a",
+//       avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/784.jpg",
+//       name: "Mrs. Rickey Ritchie",
+//       job: "Lead Functionality Consultant",
+//       content:
+//         "Amet blanditiis quis similique sint accusamus illum magni. Aperiam dolore earum. Odit natus quod cupiditate nam nihil architecto. Occaecati corrupti ipsam praesentium laborum ipsam deleniti sint cum. Optio ducimus cum corrupti dolores debitis.",
+//     },
+//     {
+//       id: "7c5bca0a82a0fb621acf9dc1",
+//       avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/432.jpg",
+//       name: "Hugh Smith",
+//       job: "Central Configuration Designer",
+//       content:
+//         "Repudiandae vitae laboriosam. Eius hic magni ex quam molestiae magnam minus. Hic beatae corporis optio distinctio eveniet provident temporibus aliquid. Pariatur alias dolores quam nobis quidem. Voluptatem reiciendis magni quos neque cum iure itaque quidem. Alias molestias id reiciendis dolorem eius.",
+//     },
+//     {
+//       id: "42ffffe92ecb90376f7b1541",
+//       avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/802.jpg",
+//       name: "Arnold Lind PhD",
+//       job: "Dynamic Web Liaison",
+//       content:
+//         "Accusantium consectetur unde accusantium asperiores nisi repellat ea delectus. Numquam atque libero sunt. Excepturi error enim dolore quos accusantium unde.",
+//     },
+//     {
+//       id: "1d1570fa3d0dfeb8fbe90a7f",
+//       avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/927.jpg",
+//       name: "Brandy Stroman",
+//       job: "Direct Marketing Producer",
+//       content:
+//         "Exercitationem quia ad error dicta provident expedita neque eum reprehenderit. Numquam illum minus neque. Sit quod officiis. Tenetur nobis tempore nostrum.",
+//     },
+//     {
+//       id: "fbe30443cc6aecdcf21dd0be",
+//       avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/607.jpg",
+//       name: "Anna Rogahn MD",
+//       job: "Central Program Director",
+//       content:
+//         "Dolore saepe cum repudiandae mollitia quia aspernatur. Vitae vel illo amet quae quis placeat optio ullam perspiciatis. Aliquid exercitationem expedita maiores numquam. Laborum magni id doloremque quibusdam quod quis explicabo beatae.",
+//     },
+//     {
+//       id: "a353b9c1f8fecbd32b0e1fee",
+//       avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/173.jpg",
+//       name: "Rufus Adams",
+//       job: "Future Functionality Analyst",
+//       content:
+//         "Error asperiores quia quaerat quos. Enim optio ducimus eius neque et doloribus exercitationem. Molestias ducimus hic eos accusamus commodi saepe. Rem soluta inventore eum dignissimos dolorem nulla quibusdam recusandae. Ullam quibusdam aperiam labore vero repellendus quia ab.",
+//     },
+//     {
+//       id: "a01b99a200f3e728faa3d8c0",
+//       avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/296.jpg",
+//       name: "Dr. Antonia Lynch",
+//       job: "Product Division Coordinator",
+//       content:
+//         "Voluptates aperiam iusto nihil quidem ad exercitationem quo reiciendis facilis. Error deserunt ullam animi hic commodi. Harum molestias rerum ratione voluptatem. Exercitationem quam repellendus eligendi saepe ex quaerat doloremque temporibus. Enim corrupti rem vitae in animi.",
+//     },
+//     {
+//       id: "bd3ed8ffa3f3be5ec7bc6a1c",
+//       avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/227.jpg",
+//       name: "Wendy Reilly II",
+//       job: "Internal Metrics Producer",
+//       content:
+//         "Eaque impedit ipsa ratione. Nisi minus doloribus provident soluta. At doloremque distinctio. Facere totam beatae ad optio consectetur ex nostrum harum consequuntur.",
+//     },
+//     {
+//       id: "16e57c53df8280cdce6a7b64",
+//       avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/379.jpg",
+//       name: "Crystal Kiehn",
+//       job: "Corporate Research Technician",
+//       content:
+//         "Tempore quos minima accusamus quibusdam veniam quod. Nemo tempore exercitationem fuga nobis. Provident fugit pariatur repellat minima iusto vel natus sunt.",
+//     },
+//   ];
+
+//   return (
+//     <div className="user-app">
+//       <h1 className="heading">User</h1>
+//       <UserList reviews={reviews} />
+//     </div>
+//   );
+
+// };
+
+// const root = ReactDOM.createRoot(document.querySelector("#app"));
+// root.render(<App />)
+
+
+///////////////////////////
+
+const Review = ({ review, onPrev, isPlay, onNext, onDown, onUp }) => {
+  return (
+    <div className="review-card">
+      <div className="review">
+        <div className="author">
+          <div className="author-avatar">
+            <img src={review.avatar} alt={review.name} />
+          </div>
+
+          <div className="author-info">
+            <h4 className="author-name">{review.singer}</h4>
+            <div className="author-job">{review.jobTitle}</div>
+
+            <div className="Song">
+              <p>{review.Song}</p>
+            </div>
+          </div>
+
+
+        </div>
+
+
+      </div>
+
+      <div className="controls">
+        <button className="btn-prev" onClick={onPrev}>
+          Prev
+        </button>
+       
+        <button className="btn-play" onClick={isPlay}>Play
+        </button>
+
+        <button className="btn-next" onClick={onNext}>
+          Next
+        </button>
+      </div>
+      <div className="volum">
+      <button className="btn-turndown" onClick={onDown}>
+          Giảm
+        </button>
+      
+        <button className="btn-turnup" onClick={onUp}>
+          Tăng
+        </button>
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
+  const [index, setIndex] = React.useState(0);
+
   const reviews = [
     {
       id: "f78ff93a2f6723ba714b938d",
-      avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/971.jpg",
-      name: "Jorge Farrell",
-      job: "Investor Marketing Producer",
-      content:
-        "Dignissimos libero magnam unde sapiente quaerat vel accusantium reprehenderit. Quam excepturi omnis. Non inventore facilis ipsum iure omnis quis sunt.",
+      avatar: "https://vcdn1-giaitri.vnecdn.net/2022/08/08/tangduytan-1659953332-2312-1659954397.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=G5hGugcaD-71nGj6dHPG1g",
+      singer: "Tăng Duy Tân",
+      Song:
+        "Bên trên tầng lầu",
     },
     {
       id: "b39e485adf7eac8ef7ccdb7a",
-      avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/784.jpg",
-      name: "Mrs. Rickey Ritchie",
-      job: "Lead Functionality Consultant",
-      content:
-        "Amet blanditiis quis similique sint accusamus illum magni. Aperiam dolore earum. Odit natus quod cupiditate nam nihil architecto. Occaecati corrupti ipsam praesentium laborum ipsam deleniti sint cum. Optio ducimus cum corrupti dolores debitis.",
+      avatar: "https://nld.mediacdn.vn/k:2015/miule-1450536907797/miu-le-cu-vo-tu-song.jpg",
+      singer: "Miu Lê, Karik, Châu Đăng Khoa",
+      Song:
+        "Vì Mẹ Anh Bắt Chia Tay",
     },
     {
       id: "7c5bca0a82a0fb621acf9dc1",
-      avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/432.jpg",
-      name: "Hugh Smith",
-      job: "Central Configuration Designer",
-      content:
-        "Repudiandae vitae laboriosam. Eius hic magni ex quam molestiae magnam minus. Hic beatae corporis optio distinctio eveniet provident temporibus aliquid. Pariatur alias dolores quam nobis quidem. Voluptatem reiciendis magni quos neque cum iure itaque quidem. Alias molestias id reiciendis dolorem eius.",
+      avatar: "https://image.thanhnien.vn/w1024/Uploaded/2022/znetns/2022_08_08/ha-nhi-7-8636.jpg",
+      singer: "Hà Nhi",
+      Song:
+        "Chưa Quên Người Yêu Cũ",
     },
     {
       id: "42ffffe92ecb90376f7b1541",
-      avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/802.jpg",
-      name: "Arnold Lind PhD",
-      job: "Dynamic Web Liaison",
-      content:
-        "Accusantium consectetur unde accusantium asperiores nisi repellat ea delectus. Numquam atque libero sunt. Excepturi error enim dolore quos accusantium unde.",
+      avatar: "https://vtv1.mediacdn.vn/thumb_w/650/2017/unnamed-5-1504884168956-1505214712018-crop-1505214720160-crop-1505214738002.jpg",
+      singer: "Mai Tiến Dũng",
+      Song:
+        "Người Như Anh",
     },
     {
       id: "1d1570fa3d0dfeb8fbe90a7f",
-      avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/927.jpg",
-      name: "Brandy Stroman",
-      job: "Direct Marketing Producer",
-      content:
-        "Exercitationem quia ad error dicta provident expedita neque eum reprehenderit. Numquam illum minus neque. Sit quod officiis. Tenetur nobis tempore nostrum.",
+      avatar: "http://image.daidoanket.vn/w640/images/upload/vanmt/01292021/ho-ngoc-ha.jpg",
+      singer: "Hồ Ngọc Hà",
+      Song:
+        "Cô Đơn Trên Sofa",
     },
     {
       id: "fbe30443cc6aecdcf21dd0be",
-      avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/607.jpg",
-      name: "Anna Rogahn MD",
-      job: "Central Program Director",
-      content:
-        "Dolore saepe cum repudiandae mollitia quia aspernatur. Vitae vel illo amet quae quis placeat optio ullam perspiciatis. Aliquid exercitationem expedita maiores numquam. Laborum magni id doloremque quibusdam quod quis explicabo beatae.",
+      avatar: "https://event.mediacdn.vn/257767050295742464/image/hot14/2021/4/1/chau-dang-khoa-33-1617253020373680922092.png",
+      singer: "Đình Dũng, ACV",
+      Song:
+        "Đế Vương",
     },
-    {
-      id: "a353b9c1f8fecbd32b0e1fee",
-      avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/173.jpg",
-      name: "Rufus Adams",
-      job: "Future Functionality Analyst",
-      content:
-        "Error asperiores quia quaerat quos. Enim optio ducimus eius neque et doloribus exercitationem. Molestias ducimus hic eos accusamus commodi saepe. Rem soluta inventore eum dignissimos dolorem nulla quibusdam recusandae. Ullam quibusdam aperiam labore vero repellendus quia ab.",
-    },
-    {
-      id: "a01b99a200f3e728faa3d8c0",
-      avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/296.jpg",
-      name: "Dr. Antonia Lynch",
-      job: "Product Division Coordinator",
-      content:
-        "Voluptates aperiam iusto nihil quidem ad exercitationem quo reiciendis facilis. Error deserunt ullam animi hic commodi. Harum molestias rerum ratione voluptatem. Exercitationem quam repellendus eligendi saepe ex quaerat doloremque temporibus. Enim corrupti rem vitae in animi.",
-    },
-    {
-      id: "bd3ed8ffa3f3be5ec7bc6a1c",
-      avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/227.jpg",
-      name: "Wendy Reilly II",
-      job: "Internal Metrics Producer",
-      content:
-        "Eaque impedit ipsa ratione. Nisi minus doloribus provident soluta. At doloremque distinctio. Facere totam beatae ad optio consectetur ex nostrum harum consequuntur.",
-    },
-    {
-      id: "16e57c53df8280cdce6a7b64",
-      avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/379.jpg",
-      name: "Crystal Kiehn",
-      job: "Corporate Research Technician",
-      content:
-        "Tempore quos minima accusamus quibusdam veniam quod. Nemo tempore exercitationem fuga nobis. Provident fugit pariatur repellat minima iusto vel natus sunt.",
-    },
+    
   ];
 
+  const handleNext = () => {
+    if (index == reviews.length - 1) setIndex(0);
+    else setIndex(index + 1);
+  };
+
+  const handlePrev = () => {
+    if (index == 0) setIndex(reviews.length - 1);
+    else setIndex(index - 1);
+  };
+
+  const review = reviews[index];
+
   return (
-    <div className="user-app">
-      <h1 className="heading">User</h1>
-      <UserList reviews={reviews} />
+    <div className="section-reviews">
+      {/* <h2 className="section-heading">Our Reviews</h2> */}
+
+      <Review review={review} onPrev={handlePrev} onNext={handleNext} />
     </div>
   );
-
 };
 
 const root = ReactDOM.createRoot(document.querySelector("#app"));
-root.render(<App />)
+root.render(<App />);
 
 
 
