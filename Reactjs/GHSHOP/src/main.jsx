78666dea367a51fe91d97e85337fe8adf711272f
dinhtrongkhang <khangdinhtb@gmail.com>
2022-11-14 05:16:37 +0700

    khangdinh update pages

--- Reactjs/GHSHOP/src/main.jsx
+++ Reactjs/GHSHOP/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { BrowserRouterm, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import Baby from './pages/baby/Baby';
+import Women from './pages/women/Women';
+import Men from './pages/men/Men';
+import New from './pages/news/New';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='baby' element={<Baby />} />
+        <Route path='women' element={<Women />} />
+        <Route path='men' element={<Men />} />
+        <Route path='news' element={<New />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
