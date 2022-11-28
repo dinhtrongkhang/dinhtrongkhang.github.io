9deba60dd764553ffe1d2ce3a020f18167b4e58a
dinhtrongkhang <khangdinhtb@gmail.com>
2022-11-28 17:26:20 +0700

    khangdinh update cart

--- ghshop-v4/src/pages/cart/Cart.jsx
+++ ghshop-v4/src/pages/cart/Cart.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Stack, Table, Button, ButtonGroup } from "react-bootstrap";
+import {Container, Row, Stack, Table, Button, ButtonGroup } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { selectCart } from "../../store/features/cart/cart.slice";
 
@@ -28,76 +28,80 @@ const Cart = () => {
 
     return (
         <div className="container">
-            <p>Giỏ hàng</p>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Hình ảnh</th>
-                        <th>Thông tin</th>
-                        <th>Số lượng</th>
-                        <th></th>
-                    </tr>
-                </thead>
+            <Container className="mt-5">
+                <Row>
+                    <p>Giỏ hàng</p>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Hình ảnh</th>
+                                <th>Thông tin</th>
+                                <th>Số lượng</th>
+                                <th></th>
+                            </tr>
+                        </thead>
 
-                <tbody>
-                    {items.map((item) => (
-                        <tr key={item.product.id}>
-                            <td>
-                                <img width={200} src={item.product.image} />
-                            </td>
-                            <td>
-                                <Stack direction="vertical">
-                                    <span>{item.product.title}</span>
-                                    <span>${item.product.price}</span>
-                                </Stack>
-                            </td>
-                            <td>
-                                <ButtonGroup>
-                                    <Button
-                                        variant="light"
-                                        onClick={() =>
-                                            dispatch(decQty(item.product.id))
-                                        }
-                                    >
-                                        -
-                                    </Button>
-                                    <Button variant="light">
-                                        {item.quantity}
-                                    </Button>
-                                    <Button
-                                        variant="light"
-                                        onClick={() =>
-                                            dispatch(incQty(item.product.id))
-                                        }
-                                    >
-                                        +
-                                    </Button>
-                                </ButtonGroup>
-                            </td>
-                            <td>
-                                <Button
-                                    onClick={() => {
-                                        handleDelete(item.product.id);
-                                    }}
-                                >
-                                    Xóa
-                                </Button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
+                        <tbody>
+                            {items.map((item) => (
+                                <tr key={item.product.id}>
+                                    <td>
+                                        <img width={200} src={item.product.image} />
+                                    </td>
+                                    <td>
+                                        <Stack direction="vertical">
+                                            <span>{item.product.title}</span>
+                                            <span>${item.product.price}</span>
+                                        </Stack>
+                                    </td>
+                                    <td>
+                                        <ButtonGroup>
+                                            <Button
+                                                variant="light"
+                                                onClick={() =>
+                                                    dispatch(decQty(item.product.id))
+                                                }
+                                            >
+                                                -
+                                            </Button>
+                                            <Button variant="light">
+                                                {item.quantity}
+                                            </Button>
+                                            <Button
+                                                variant="light"
+                                                onClick={() =>
+                                                    dispatch(incQty(item.product.id))
+                                                }
+                                            >
+                                                +
+                                            </Button>
+                                        </ButtonGroup>
+                                    </td>
+                                    <td>
+                                        <Button
+                                            onClick={() => {
+                                                handleDelete(item.product.id);
+                                            }}
+                                        >
+                                            Xóa
+                                        </Button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
 
-                <tfoot>
-                    <tr>
-                        <td colSpan={3}>Tổng tiền</td>
-                        <td>${totalPrice}</td>
-                    </tr>
-                </tfoot>
-            </Table>
+                        <tfoot>
+                            <tr>
+                                <td colSpan={3}>Tổng tiền</td>
+                                <td>${totalPrice}</td>
+                            </tr>
+                        </tfoot>
+                    </Table>
 
-            <Button type="danger" onClick={handleClear}>
-                Xóa toàn bộ sản phẩm
-            </Button>
+                    <Button type="danger" onClick={handleClear}>
+                        Xóa toàn bộ sản phẩm
+                    </Button>
+                </Row>
+            </Container>
         </div>
     );
 };
