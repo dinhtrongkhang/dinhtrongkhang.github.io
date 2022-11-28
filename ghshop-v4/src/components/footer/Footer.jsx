333b998d120a132bb88457f11221f552b8a1e318
dinhtrongkhang <khangdinhtb@gmail.com>
2022-11-28 15:50:31 +0700

    khangdinh update footer

--- ghshop-v4/src/components/footer/Footer.jsx
+++ ghshop-v4/src/components/footer/Footer.jsx
@@ -1,16 +1,56 @@
 import React from "react";
-import { Container, Stack } from "react-bootstrap";
+import { Container, Row, Col, Stack } from "react-bootstrap";
+
+import styles from "../footer/Footer.module.css"
 
 const Footer = () => {
     return (
-        <footer className="py-5">
-            <Container>
-                <Stack
-                    direction="horizontal"
-                    className="justify-content-center"
-                >
-                    Dinh Gia Huy
-                </Stack>
+        <footer>
+            <Container fluid>
+                <Row className="justify-content-betwen" className={styles.footerTop}>
+                    <Col xs={12} md={6} lg={4} xl={3}>
+                        <Stack className="text-start mt-3">
+                            <h3>Lorem, ipsum dolor.</h3>
+                            <p>Lorem, ipsum.</p>
+                            <p>Lorem, ipsum.</p>
+                            <p>Lorem, ipsum.</p>
+                            <p>Lorem, ipsum.</p>
+                        </Stack>
+                    </Col>
+                    <Col xs={12} md={6} lg={4} xl={3}>
+                        <Stack className="text-start mt-3">
+                            <h3>Lorem, ipsum dolor.</h3>
+                            <p>Lorem, ipsum.</p>
+                            <p>Lorem, ipsum.</p>
+                            <p>Lorem, ipsum.</p>
+                            <p>Lorem, ipsum.</p>
+                        </Stack>
+                    </Col>
+                    <Col xs={12} md={6} lg={4} xl={3}>
+                        <Stack className="text-start mt-3">
+                            <h3>Lorem, ipsum dolor.</h3>
+                            <p>Lorem, ipsum.</p>
+                            <p>Lorem, ipsum.</p>
+                            <p>Lorem, ipsum.</p>
+                            <p>Lorem, ipsum.</p>
+                        </Stack>
+                    </Col>
+                    <Col xs={12} md={6} lg={4} xl={3}>
+                        <Stack className="text-start mt-3">
+                            <h3>Lorem, ipsum dolor.</h3>
+                            <p>Lorem, ipsum.</p>
+                            <p>Lorem, ipsum.</p>
+                            <p>Lorem, ipsum.</p>
+                            <p>Lorem, ipsum.</p>
+                        </Stack>
+                    </Col>
+                </Row>
+            </Container>
+
+            <Container fluid>
+                <Row>
+                    <p className={styles.copyRight}>Bản quyền thuộc về GHSHOP</p>
+                </Row>
             </Container>
         </footer>
     );
