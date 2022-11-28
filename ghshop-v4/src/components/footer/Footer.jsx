eac863f4af6aa82f00bf0aef70ac49cfe0168d20
dinhtrongkhang <khangdinhtb@gmail.com>
2022-11-28 16:19:01 +0700

    khangdinh update footer

--- ghshop-v4/src/components/footer/Footer.jsx
+++ ghshop-v4/src/components/footer/Footer.jsx
@@ -11,37 +11,37 @@ const Footer = () => {
                     <Col xs={12} md={6} lg={4} xl={3}>
                         <Stack className="text-start mt-3">
                             <h4 className={styles.footerTitle}>Liên hệ với chúng tôi</h4>
-                            <p>Địa chỉ: An Ninh, Tiền Hải, Thái Bình</p>
-                            <p>Hotline: +84-9043 39993</p>
-                            <p>Email: khangdinhtb@gmail.com</p>
-                            <p>Giờ mở cửa: Hai - Chủ Nhật: 9am-7pm</p>
+                            <p><a href="">Địa chỉ: An Ninh, Tiền Hải, Thái Bình</a></p>
+                            <p><a href="">Hotline: +84-9043 39993</a></p>
+                            <p><a href="">Email: khangdinhtb@gmail.com</a></p>
+                            <p><a href="">Giờ mở cửa: Hai - Chủ Nhật: 9am-7pm</a></p>
                         </Stack>
                     </Col>
                     <Col xs={12} md={6} lg={4} xl={3}>
                         <Stack className="text-start mt-3">
                             <h4 className={styles.footerTitle}>Chăm sóc khách hàng</h4>
-                            <p>Chính sách điều khoản</p>
-                            <p>Hướng dẫn mua hàng</p>
-                            <p>Chính sách thanh toán</p>
-                            <p>Chính sách giao nhận và vận chuyển</p>
+                            <p><a href="">Chính sách điều khoản</a></p>
+                            <p><a href="">Hướng dẫn mua hàng</a></p>
+                            <p><a href="">Chính sách thanh toán</a></p>
+                            <p><a href="">Chính sách giao nhận và vận chuyển</a></p>
                         </Stack>
                     </Col>
                     <Col xs={12} md={6} lg={4} xl={3}>
                         <Stack className="text-start mt-3">
                             <h4 className={styles.footerTitle}>Thông tin</h4>
-                            <p>Về GHSHOP</p>
-                            <p>Tuyển dụng</p>
-                            <p>Hệ thống cửa hàng</p>
-                            <p>Lorem, ipsum.</p>
+                            <p><a href="">Về GHSHOP</a></p>
+                            <p><a href="">Tuyển dụng</a></p>
+                            <p><a href="">Hệ thống cửa hàng</a></p>
+                            <p><a href="">Lorem, ipsum.</a></p>
                         </Stack>
                     </Col>
                     <Col xs={12} md={6} lg={4} xl={3}>
                         <Stack className="text-start mt-3">
                             <h4 className={styles.footerTitle}>Theo dõi chúng tôi</h4>
-                            <p>Lorem, ipsum.</p>
-                            <p>Lorem, ipsum.</p>
-                            <p>Lorem, ipsum.</p>
-                            <p>Lorem, ipsum.</p>
+                            <p><a href="">Lorem, ipsum.</a></p>
+                            <p><a href="">Lorem, ipsum.</a></p>
+                            <p><a href="">Lorem, ipsum.</a></p>
+                            <p><a href="">Lorem, ipsum.</a></p>
                         </Stack>
                     </Col>
                 </Row>
@@ -49,7 +49,7 @@ const Footer = () => {
 
             <Container fluid>
                 <Row>
-                    <span className={styles.copyRight}>Copyright © 2022 by GHSHOP.</span>
+                    <span className={styles.copyRight}>Copyright © 2022 by GHSHOP</span>
                 </Row>
             </Container>
         </footer>
